cd50e1cf88dbf0e766f6f725de7b52ea69c3368b
Deepak Pal <krishnamay@users.noreply.github.com>
2018-03-07 14:09:51 +0530

    code update

--- php-sdk/createfile.php
+++ php-sdk/createfile.php
@@ -7,8 +7,8 @@ include 'connecttoaws.php';
 /*
 Files in Amazon S3 are called "objects" and are stored in buckets. A specific
 object is referred to by its key (or name) and holds data. In this file
-we create an object called acloudguru.txt that contains the data 
-'Hello Cloud Gurus!'
+we create an object called acloudfolks.txt that contains the data 
+'Hello Cloud Folkss!'
 and we upload/put it into our newly created bucket.
 */
 
@@ -16,17 +16,17 @@ and we upload/put it into our newly created bucket.
 $bucket = $_GET["bucket"];
 
 //create the file name
-$key = 'cloudguru.txt';
+$key = 'cloudfolks.txt';
 
 //put the file and data in our bucket
 $result = $client->putObject(array(
     'Bucket' => $bucket,
     'Key'    => $key,
-    'Body'   => "Hello Cloud Gurus!"
+    'Body'   => "Hello Cloud Folks!"
 ));
 
 //HTML to create our webpage
 echo "<h2 align=\"center\">File - $key has been successfully uploaded to $bucket</h2>";
-echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised.png\"></img></div>";
+echo "<div align = \"center\"><img src=\"https://s3.ap-south-1.amazonaws.com/mphasis-hpsbu/CloudFolks/cloudFolks.JPG\"></img></div>";
 echo "<div align = \"center\"><a href=\"readfile.php?bucket=$bucket&key=$key\">Click Here To Read Your File</a></div>";
 ?>
